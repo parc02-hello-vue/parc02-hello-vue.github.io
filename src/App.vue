f51c77cce771c2c6c2a04613de70c011c1d46f5b
parc02 <pascalmin49@gmail.com>
2024-01-31 15:02:36 +0900

    데이터 import 해오기

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <script setup>
-import { ref } from 'vue'
+import {ref} from 'vue'
+import category from "@/assets/category.json"
 
-const awesome = ref(true)
-
-console.log(awesome.valie)
-
-function toggleAwesome(v)
-{
-    awesome.value =!v
-}
-
-
-
-function isAwesome()
-{
-    return awesome.value
-}    
-
-    </script>
+const categories = ref(category);
+</script>
 
 <template>
-    <button @click ="toggleAwesome(awesome)">toggle</button>
+    <div>
+	<ul>
+	    <li v-for="category in categories" :key="category.code">
+		<span>{{ category.name }}</span>
+		<span>{{ category.code }}</span>
+	    </li>
+	</ul>
+    </div>
+</template>
 
-    <h1 v-if ="isAwesome()"> Vue is awesome!</h1>
-    <h1 v-else> Oh no Vue isn't awesome 😭😭😭</h1>
 
-    <h2 v-if="isAwesome()">TRUE</h2>
-    <h2 v-else>FALSE</h2>
-    </template>
