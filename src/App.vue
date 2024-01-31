ca0a56c198801e074bf8fcfc98cffa62b54dc1e2
parc02 <pascalmin49@gmail.com>
2024-01-31 11:39:27 +0900

    hello cnt + add name

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,30 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {ref, onMounted} from 'vue'
+
+//reactive state
+const count = ref({
+count:0, 
+name: "PA",
+})
+    
+//function that mutate state and trigger updates 
+function increment(){
+count.value.count=count.value.count+100
+    count.value.name=count.value.name+"HA"
+}
+
+//lifecycle hooks
+onMounted(() => {
+	console.log(`The initial count is ${count.value}.`)
+	})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+<button @click="increment">Count is: {{ count.count }} {{count.name}}</button>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+button {
+    font-weight:bold;
 }
 </style>
