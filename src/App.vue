cca8eeb87c6fb4ad794d04e63b633e86df37eb8a
parc02 <pascalmin49@gmail.com>
2024-02-01 16:19:16 +0900

    child-parent

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Child from './components/Child.vue'
+import { ref} from 'vue'
 
+const pocketMoney = ref('부모 컴포넌트로부터 <📩>를 받았어요!')
 </script>
 
 <template>
-<Child/>
+<Child :pocketMoney="pocketMoney"/>
 </template>
 
