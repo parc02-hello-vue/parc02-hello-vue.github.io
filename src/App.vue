c2cf96aa49024caa4fab3296a49c97fed2df9c05
parc02 <pascalmin49@gmail.com>
2024-02-01 10:24:34 +0900

    react 사용

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,18 @@ const author = ref({
 name:'John Doe',
 books: [1,2,3] 
 })
+
+const check = computed(() => {
+	return author.value.books.length >2 ? 'Yes' : 'No'
+	})
 </script>
 
+
+
 <template>
-<p>Has published books:</p>
-<span>{{ author.books.length > 2 ? 'Yes':'No'}}</span>
+<p>🤠🤓🐲🦖:</p>
+<span>{{author.books.length>2?'Yes':'No'}}</span>
+<div>computed⬇️:</div>
+<div>{{check}}</div>
 </template>
 
