01e1aa441288c5916eccba78289c8b4a80de47a1
parc02 <pascalmin49@gmail.com>
2024-02-01 10:47:43 +0900

    onUpdated

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-import {ref} from 'vue'
-
-const message = ref ('')
+import {ref, onUpdated} from 'vue'
+const count = ref(0)
+    onUpdated(() => {
+	    console.log(document.getElementById('count').textContent)
+	    })
 </script>
+
 <template>
-<span> Multi-line message is:</span>
-<p style = "white-space: pre-line">{{message}}</p>
-<textarea v-model="message" placeholder="add multiple lines🦖"></textarea>`
+<button id='count' @click="count++">{{count}}</button>
 </template>
 
