b8572890b2978beb4d9df55e499def67438ab052
parc02 <pascalmin49@gmail.com>
2024-02-01 10:28:13 +0900

    function say button

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <script setup>
-import {reactive, computed, ref} from 'vue'
-
-const author = ref({
-name:'John Doe',
-books: [1,2,3] 
-})
-
-const check = computed(() => {
-	return author.value.books.length >2 ? 'Yes' : 'No'
-	})
+function say(message) {
+    alert(message)
+}
 </script>
-
-
-
 <template>
-<p>🤠🤓🐲🦖:</p>
-<span>{{author.books.length>2?'Yes':'No'}}</span>
-<div>computed⬇️:</div>
-<div>{{check}}</div>
+<button @click = "say('hello')">Say Hello </button>
+<button @click = "say('bye')">Say Bye </button>
+
 </template>
 
