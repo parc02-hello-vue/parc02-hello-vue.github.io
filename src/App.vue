5e0d3203b920547c8f41b489a5c04cfe43d996a6
parc02 <pascalmin49@gmail.com>
2024-02-01 11:12:45 +0900

    onMounted

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
-import {ref, onUpdated} from 'vue'
+import {ref, onUpdated, onMounted, onBeforeUpdate} from 'vue'
 const count = ref(0)
     onUpdated(() => {
 	    console.log(document.getElementById('count').textContent)
 	    })
+onMounted(() =>{
+	console.log(`the componet is mounted`)
+	})
 </script>
 
 <template>
-<button id='count' @click="count++">{{count}}</button>
+<button id='count' @click="count=count+10">🦖{{count}}🦖</button>
 </template>
 
